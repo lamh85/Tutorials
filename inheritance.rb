88626d7e14a4d925c68481ab415f61f1ba05612a
Hon Lam <hon@junglescout.com>
2019-05-22 22:27:57 -0700

    Add notes to inheritance

--- inheritance.rb
+++ inheritance.rb
@@ -43,3 +43,42 @@ end
 class Mushroom < Item
 end
 # If the hierarchy starts out with too many concrete attributes in Item, then the clients of the hierarchy will know too much about Coin and Mushroom.
+
+# Example of too much centralization in the superclass:
+class Organism
+  def some_list
+    ["apple", "banana", "grape"]
+  end
+end
+class Human < Organism
+  def some_list
+    super.select do |item| item != "banana" end
+  end
+end
+class Dog < Organism
+  def some_list
+    [*super, "mango"]
+  end
+end
+# Problem: the subclasses know too much about the superclass:
+  # It knows that the default value is an array.
+  # It knows that there is a method called some_list.
+
+# Raise an error if a subclass doesn't have a method, but is required.
+# Use this pattern if the superclass does not have a default, but its subclasses must have the method.
+class Organism
+  def some_method
+    raise NotImplementedError, "This #{self.class} cannot respond to:"
+  end
+end
+
+# Pitfall: Making this required in all subclasses:
+def initialize
+  # some code
+  
+  super
+end
+# Why? Adding a new subclass could forget to call super. Then the default attributes won't be set.
+
+# BIG TAKEAWAY: `super` is always a dependency
+
