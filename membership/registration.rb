15ec825ad0e6e20466e1bac794894d8f7af44d9b
Hon Lam <hon@junglescout.com>
2019-08-23 16:34:42 -0700

    Registration code.

--- membership/registration.rb
+++ membership/registration.rb
@@ -1,17 +1,9 @@
 module RegisterUser
-  class Coordinator
+
+  # SUPER ABSTRACT - IE - Should rarely ever change
+  class Tasks < ::MembershipTasks
     class << self
-      
-
-      private
-
-      def run_tasks
-        tasks.each do |task|
-          task.call(registration_event)
-        end
-      end
-
-      def tasks
+      def list
         [
           CreateStripeCustomer,
           CreateUser,
@@ -22,5 +14,47 @@ module RegisterUser
     end
   end
 
+  class Coordinator < ::TaskCoordinator
+    attr_reader :event
+    attr_accessor :intertask_payload, :current_task, :task_result
 
+    def initialize(event)
+      @current_task = ''
+      @task_result = {}
+      @event = event
+      @intertask_payload = {}
+    end
+
+    def run
+      new(event).run_task_list
+    end
+
+    private
+
+    def run_task_list
+      Tasks.list.each do |task|
+        run_task
+      end
+
+      # Do something if succeeds?
+    end
+
+    def run_task
+      current_task = task.to_s
+      task_result = task.run(event, intertask_payload)
+      handle_failed_task if (task_result[:status] == :error)
+      intertask_payload[current_task] = task_result[:payload]
+    end
+
+    def handle_failed_task
+      # raise with following info:
+        # task_result
+        # current_task
+    end
+  end
+
+  class CreateMembership
+    def run
+    end
+  end
 end
