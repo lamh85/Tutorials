b0627178e76b1e9623b29b4ea452a628d4ee7aa8
Hon Lam <hon@junglescout.com>
2019-08-26 01:17:02 -0700

    More registration code.

--- membership/registration.rb
+++ membership/registration.rb
@@ -1,7 +1,16 @@
-module RegisterUser
+# Possible tasks
+  # Database CRUD
+    # user
+    # membership
+    # access right
+  # Stripe CRUD
+    # customer
+    # subscription
+    # source
 
-  # SUPER ABSTRACT - IE - Should rarely ever change
-  class Tasks < ::MembershipTasks
+# SUPER ABSTRACT - IE - Should rarely ever change
+module TaskList
+  class Registration < ::MembershipTaskList
     class << self
       def list
         [
@@ -13,48 +22,53 @@ module RegisterUser
       end
     end
   end
+end
 
-  class Coordinator < ::TaskCoordinator
-    attr_reader :event
-    attr_accessor :intertask_payload, :current_task, :task_result
+class TaskCoordinator
+  attr_reader :event, :task_list
+  attr_accessor :intertask_payload, :current_task, :task_result
 
-    def initialize(event)
-      @current_task = ''
-      @task_result = {}
-      @event = event
-      @intertask_payload = {}
-    end
-
-    def run
-      new(event).run_task_list
-    end
-
-    private
-
-    def run_task_list
-      Tasks.list.each do |task|
-        run_task
-      end
-
-      # Do something if succeeds?
-    end
-
-    def run_task
-      current_task = task.to_s
-      task_result = task.run(event, intertask_payload)
-      handle_failed_task if (task_result[:status] == :error)
-      intertask_payload[current_task] = task_result[:payload]
-    end
-
-    def handle_failed_task
-      # raise with following info:
-        # task_result
-        # current_task
-    end
+  def initialize(event, task_list)
+    @current_task = ''
+    @task_result = {}
+    @event = event
+    @intertask_payload = {}
+    @task_list = task_list
   end
 
-  class CreateMembership
-    def run
+  def call
+    new(event).run_task_list
+  end
+
+  private
+
+  def run_task_list
+    ActiveRecord::Base.transaction do
+      task_list.list.each do |task|
+        begin
+          run_task
+        rescue
+          handle_failed_task
+        end
+      end
     end
+
+    # Do something if succeeds?
+  end
+
+  def run_task
+    current_task = task.to_s
+    task_result = task.run(event, intertask_payload)
+    handle_failed_task if (task_result[:status] == :error)
+    intertask_payload[current_task] = task_result[:payload]
+  end
+
+  def handle_failed_task
+    # raise with following info:
+      # task_result
+      # current_task
+  end
+
+  def report_sentry
   end
 end
